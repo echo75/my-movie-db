af3dc824c84cf7075ea438c32deeb83c4f556d36
Johan Hedman <137423199+johan-hedman@users.noreply.github.com>
2023-07-27 11:56:20 +0000

    adding pseudo-getters for all User-methods

--- src/test.js
+++ src/test.js
@@ -20,9 +20,16 @@ class Test {
 
     console.log(morgan.removeWatch(this.movieSource, 'tt0473444')) // Curse of the Golden Flower - remove from watch
     console.log(morgan.removeWatched(this.movieSource, 'tt0071807')) // The man with the golden gun - remove from watched
-    morgan.rate(9, 'tt0091129', 1) // rate the movie with id 1, rating 9, imdbID 6, userId 1
-    morgan.rate(3, 'tt0071807', 1) // rate the movie with id 1, rating 9, imdbID 6, userId 1
+    morgan.rate(9, 'tt0091129', 1) // rate the movie with: rating 9, imdbID, userId 1
+    morgan.rate(3, 'tt0091129', 2) // rate the movie with: rating 3, imdbID, userId 2
+    morgan.rate(1, 'tt0091129', 3) // rate the movie with: rating 3, imdbID, userId 3
+    morgan.rate(4, 'tt0071807', 2) // rate the movie with: rating 4, imdbID, userId 2
     morgan.review('"The Golden Child" is a great movie', 'tt0091129', 1) // review 'This is a great movie', imdbID: tt0091129 , userId 1
+    morgan.review('"The Golden Child" is watchable but not too good', 'tt0091129', 2) // review 'This is a great movie', imdbID: tt0091129 , userId 1
+    console.log(morgan.onWatch()) // display watch list
+    console.log(morgan.onWatched()) // display watched list
+    console.log(morgan.reviewsMovie('tt0091129')) // display reviews for movie with imdbID tt0091129
+    console.log(morgan.ratingsAverage('tt0091129')) // display average rating for movie with imdbID tt0091129
     console.log(morgan)
   }
 }
