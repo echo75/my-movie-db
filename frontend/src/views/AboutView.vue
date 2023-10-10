05fb41f9fd9e9fc259c5d0eec9482e05c61d2573
Johan Hedman <johan@dreamsland.de>
2023-10-10 14:34:50 +0000

    Test "About this App" on page About

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <script></script>
 <template lang="pug">
-h1 About
+h1 About this App
     //- p(v-if="user") You are logged in as {{ user?.data?.email }} p(v-if="user") Logged in as:
     //- {{ user?.email }}
 
